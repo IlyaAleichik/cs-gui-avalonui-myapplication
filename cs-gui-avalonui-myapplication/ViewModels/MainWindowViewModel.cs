25a53b4f3d6db78425f108974fb9b37611950790
IlyaAleichik <ilya.alejchik@outlook.com>
2022-01-26 14:53:21 +0300

    add project file

---
+++ cs-gui-avalonui-myapplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApplicationMVVMDev.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+        public string Title => "Base manage elemenets WPF";
+
+    }
+}
---
+++ cs-gui-avalonui-myapplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using MyApplicationMVVMDev.Views;
+
+namespace MyApplicationMVVMDev.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+        public void test()
+        {
+
+        }
+    }
+}
